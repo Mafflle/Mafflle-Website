081304ea85fa222764943e0020884e83ec45391d
n3-rd <godwinjdayo@gmail.com>
2023-09-22 00:26:53 +0100

    Proper gradient

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,7 +1,12 @@
 <script>
 	import HeroElements from './sub-components/HeroElements.svelte';
+	import HeroVector from './sub-components/HeroVector.svelte';
 </script>
 
-<div class="h-[38.125rem] w-screen hero text-white flex items-center px-[50px]">
-	<HeroElements />
+<div class="overflow-hidden">
+	<HeroVector />
+
+	<div class="h-[38.125rem] w-screen hero text-white flex items-center px-[50px]">
+		<HeroElements />
+	</div>
 </div>
