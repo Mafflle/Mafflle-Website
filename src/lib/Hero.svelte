2a754ccef9d2ff971b849ca2c32d26069c489717
n3-rd <godwinjdayo@gmail.com>
2023-09-23 01:17:10 +0100

    added Hero section (pending correct gradient)

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -1,1 +1,7 @@
-<div class="h-[38.125rem] w-screen hero" />
+<script>
+	import HeroElements from './sub-components/HeroElements.svelte';
+</script>
+
+<div class="h-[38.125rem] w-screen hero text-white flex items-center px-[50px]">
+	<HeroElements />
+</div>
---
+++ src/lib/sub-components/HeroElements.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col gap-[3.625rem]">
+	<div class="text flex flex-col gap-[1.5rem] font-[400]">
+		<div class="text-[2.875rem] w-[70%] leading-[140%]">
+			We help businesses and brands across industries build a better digital identity
+		</div>
+		<div class="text-[1rem] w-[40%] leading-[160%]">
+			Your partner in shaping exceptional online identities. Seamlessly bringing your ideas to life
+		</div>
+	</div>
+	<div class="button">
+		<button class="text-[0.875rem] text-[#201F1F] font-[500] px-[3.125rem] py-[0.625rem] bg-white"
+			>Hire us today</button
+		>
+	</div>
+</div>
