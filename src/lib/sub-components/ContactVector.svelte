ba99959f7068fad27611a373c8de9584a77b6a30
n3-rd <godwinjdayo@gmail.com>
2023-10-01 19:41:56 +0100

    replaced contact background

--- src/lib/sub-components/ContactVector.svelte
+++ src/lib/sub-components/ContactVector.svelte
@@ -1,1 +1,3 @@
-<div class="w-screen h-screen absolute z-[1] bg-[url('/img/Background.svg')] bg-cover bg-center" />
+<div
+	class="w-screen h-screen absolute z-[1] bg-[url('/img/Background-2.svg')] bg-cover bg-center"
+/>
