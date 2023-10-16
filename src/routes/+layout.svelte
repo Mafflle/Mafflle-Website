3961441ad8cf7fc283b1007ed3f9e8f7c109d4e8
n3rd <godwinjdayo@gmail.com>
2023-10-17 00:34:13 +0100

    👌 IMPROVE: favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -51,6 +51,11 @@
 		content="Mafflle studios help businesses and brands across industries build a better digital identity"
 	/>
 	<meta name="twitter:image" content="https://maffle-website.vercel.app/img/intro-image.png" />
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+	<link rel="manifest" href="/icons/site.webmanifest" />
 </svelte:head>
 
 <style>
